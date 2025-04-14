260bd5393dd33383027be0d529188910f94974d0
Bernardkj <asareannanb@gmail.com>
2025-04-13 22:03:30 -0400

    new changes

---
+++ testscript.js
@@ -0,0 +1,7 @@
+    function toggleMenu() {
+      document.getElementById('navMenu').classList.toggle('show');
+    }
+    
+    function toggleDarkMode() {
+      document.body.classList.toggle('dark-mode');
+    }
